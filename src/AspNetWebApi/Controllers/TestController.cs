a5f6cb9410fe68a96d974f8c0a974e829f4ac24e
Bartłomiej Górecki <bartlomiej.gorecki@pracuj.pl>
2016-03-02 11:49:59 +0100

    asp net web api project with owin

---
+++ src/AspNetWebApi/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace AspNetWebApi.Controllers
+{
+    [RoutePrefix("test")]
+    public class TestController : ApiController
+    {
+        [Route("get")]
+        public IHttpActionResult Get()
+        {
+            return Ok("testowane");
+        }
+    }
+}
---
+++ src/AspNetWebApi/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using AspNetWebApi;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace AspNetWebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+        }
+    }
+}
