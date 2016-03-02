3f0a1c33234501393d1a583694f1a492034d8ff7
Bartłomiej Górecki <bartlomiej.gorecki@pracuj.pl>
2016-03-02 11:57:43 +0100

    multipart data content

---
+++ src/AspNetWebApi/Controllers/MultipartDataController.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AspNetWebApi.Controllers
+{
+    [RoutePrefix("multipart")]
+    public class MultipartDataController : ApiController
+    {
+        [Route("post")]
+        public async Task<IHttpActionResult> Post()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                return BadRequest();
+            }
+
+            var multipartMemoryStreamProvider = await Request.Content.ReadAsMultipartAsync();
+
+            foreach (var content in multipartMemoryStreamProvider.Contents)
+            {
+                if (content.Headers.ContentDisposition.Name == "model")
+                {
+                    var model = await content.ReadAsAsync<Model>();
+                }
+
+                if (content.Headers.ContentDisposition.Name == "content")
+                {
+                    var contentBytes = await content.ReadAsByteArrayAsync();
+                }
+            }
+
+            return Ok("parsed!");
+        }
+
+        private class Model
+        {
+            public string Id { get; set; }
+
+            public string Whatever { get; set; }
+        }
+    }
+}
